7ea855233045dd939b023d2128bab6641dfb908a
Mukund Ajmera <mukundajmera94@gmail.com>
2018-02-14 13:14:57 +0530

    with check for crud app

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ var mongo;
 //mongo db connectivity
 	//including mongodb as the package for the process
 	var mongodb = require('mongodb').MongoClient;
-	var URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/app';
+	var URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/';
 	mongodb.connect(URL,function(error,db) {
 	//connecting to the db if not throwing error
 		if(error)
@@ -11,29 +11,159 @@ var mongo;
 		}
 		else
 		{
-			mongo = db.db('app');
+			mongo = db.db('student');
 			console.log('connected to DB');
 			
 		}
 	});
 
 
-//login page
+
+///login page
 exports.login = function (req,res) {
 
-	res.render('index.ejs');
+	res.render('index.ejs',{error:null});
 
 }
 
-//admin
-exports.login_admin = function (req,res) {
+//main index page
+exports.home = function (req,res) {
 
-	res.render('index_admin.ejs');
+//validation for session
+		check(req,res);
 
+		res.render('home.ejs',{"info":req.session.user});
+	
 }
 
 
-//block for inserting the data
+
+exports.logout = function (req,res) {
+
+	check(req,res);
+
+req.session.destroy(function(err) {
+  if(err) {
+    console.log(err);
+  } 
+  else 
+  {
+    res.render('./',{error:"Successfully Logged Out"});
+  }
+});
+}
+
+exports.auth = function (req,res) {
+
+//making one record to find with and operator for email and password
+	mongo.collection("info").findOne({email:req.body.email,password:req.body.password} ,function (err,data) {
+	    if (err) throw err;
+	    
+	    if(data == null)
+	    {
+	    	//login failed redirecting to login fail page.
+	    	res.render("./",{error:"Login Failed"});
+	    }
+	    else
+	    {
+	    	//login success creating session and redirecting to home page.
+	    	req.session.user = data;
+	    	 res.redirect('/home');
+	    }
+
+	})
+
+}
+
+exports.signup = function (req,res) {
+
+	res.render('signup.ejs');
+}
+
+exports.register = function (req,res) {
+
+//server side validation for wrong password
+	if(req.body.password != req.body.password_c)
+	{
+	    res.render('./',{error:"Password not matching"});
+	}
+	else
+	{
+		//inserting record
+		var value = {name:req.body.name,email:req.body.email,password:req.body.password};
+		mongo.collection('info').insertOne(value,function (err,result) {
+			if(err) throw err;
+
+			//showing message in login page
+			res.render("./",{error:"Registered Successfully "});
+		})
+	}
+}
+
+exports.user_list = function (req,res) {
+
+	check(req,res);
+	mongo.collection('info').find().toArray(function(err, data){
+
+		if(err) throw err;
+
+		res.render('user.ejs',{'user':data});
+	});
+
+}
+
+exports.update = function (req,res) {
+
+//using url for parsing value 
+	var url = require('url');
+	var url_value = url.parse(req.url, true).query;
+
+//for learning purpose sending variable as json from query
+	// console.log(url_value.name);
+	// console.log(url_value.email);
+	 res.render("update.ejs",{'name':url_value.name,'email':url_value.email});
+
+}
+
+//function for updating in process
+exports.update_process = function (req,res) {
+
+	var search = { name: req.body.old_name , email : req.body.old_email };
+  	var replace = { $set: { name: req.body.name , email : req.body.email} };
+  	mongo.collection('info').updateOne(search,replace, function(err, result) {
+
+  		if (err) throw err
+  			console.log("doc updated");
+  		//after updation detirection to user page again
+  		res.redirect("/user");
+	})
+}
+
+exports.delete = function (req,res) {
+
+//taking names from URL so
+	var url = require('url');
+	var url_value = url.parse(req.url, true).query;
+
+	var value = {name:url_value.name,email:url_value.email}
+
+	mongo.collection("info").deleteOne(value,function (err,result) {
+		if (err) throw err;
+
+		res.redirect("/user");
+	})
+}
+
+//creating function for session validation
+function check(req,res) {
+	if(!req.session.user)
+	{
+		res.render('./',{error:"Please First Login"});
+	}
+}
+/*
+
+ ///block for inserting the data
 exports.insert_comments = function (req,res) {
 	//inserting into the collections comments
 	var data = {
@@ -70,3 +200,4 @@ exports.view = function (req,res) {
 	  });
 
 }
+*/
--- server.js
+++ server.js
@@ -4,10 +4,20 @@ var express = require('express');
 var routes = require('./routes/route.js')
 //parsing json need this library
 var bodyParser = require('body-parser');
+
+//importing package for session
+var session = require('express-session');
+
+//path addition
+path = require('path');
 //port is for binding it
 var port = process.env.PORT || 80;
 
 var app = express();
+
+app.use(session({secret: 'mukund',resave: true,saveUninitialized: true}));
+
+
 //setting template to views as ejs
 app.set('view engine','ejs');
 
@@ -16,19 +26,53 @@ app.use(bodyParser.urlencoded({
 	extended:false
 }))
 
+//both have same meaning here either by absolute or taking through relative
+//app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static('public'));
 
-//public domain
+//index or main root page of the website
 app.get('/',routes.login);
-//admin domain
-app.get('/admin',routes.login_admin);
-
-//direct linking fro view page
-app.get('/view',routes.view);
+//link for home page
+app.get('/home',routes.home);
 
 
-//to get comments from post request and call same name insert_comments fx
-app.post('/insert_comments',routes.insert_comments);
+
+//direct linking for logout page
+app.get('/logout',routes.logout);
+
+app.post('/login_check',routes.auth)
+
+//list all user details
+app.get('/user',routes.user_list)
+
+
+//dispay requ from update page
+app.get('/update',routes.update)
+
+
+//display requ from update page
+app.post('/updating',routes.update_process)
+
+//for signing in the user
+app.get('/signup',routes.signup);
+
+//for inserting the user info in the db
+app.post('/signup',routes.register);
+
+
+app.get('/delete',routes.delete);
+
+// to get comments from post request and call same name insert_comments fx
+// app.post('/insert_comments',routes.insert_comments);
 
 app.listen(port,function(){
 	console.log('Listening on port '+port);
 })
+
+
+
+// //making session availabe in all the templates 
+// app.use(function(req, res, next) {
+//   res.locals.user = req.session.user;
+//   next();
+// });
